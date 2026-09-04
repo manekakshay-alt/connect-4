f5f9f2f304efe280e988ccebc3510265bbbb40f1
ri5olu <ri5olu239@gmail.com>
2026-09-04 12:05:06 +0100

    Update backEnd.py

--- backEnd.py
+++ backEnd.py
@@ -1,6 +1,6 @@
 from random import randint
 from botFunctions1 import *#;
-from botFuncitons2 import *#;
+from botFunctions2 import *#;
 Board=[["N","N","N","N","N","N"],
        ["N","N","N","N","N","N"],
        ["N","N","N","N","N","N"],
